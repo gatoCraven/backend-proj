d5506562e284ce8267319a29c54236ecba818d25
Samkit <samkitjai09@gmail.com>
2024-01-30 00:20:55 +0530

    Connected DB

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+async function  connectDB() {
+    try{
+        const dbInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("Connected to the database");
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+export default connectDB;
+
